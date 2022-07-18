584f647ac07b062007d37b6915cf7d88a8ec7752
401 <401@academicos.cesde.edu.co>
2022-07-18 15:48:08 -0500

    Cliente

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './shared/menu/menu.component';
+import { VehiculosComponent } from './vehiculos/vehiculos.component';
+import { HomeComponent } from './home/home.component';
+import { FooterComponent } from './shared/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    VehiculosComponent,
+    HomeComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
